b04bf8132488595770436da23494dd710ef19f17
AbedAlrahmanAldade <abed.alrahman.aldade@iem.fraunhofer.de>
2025-01-09 13:08:41 +0100

    Update StreamlitAudioRecorder.tsx
    removed unnecessary components

--- st_audiorec/frontend/src/StreamlitAudioRecorder.tsx
+++ st_audiorec/frontend/src/StreamlitAudioRecorder.tsx
@@ -10,159 +10,76 @@ import 'audio-react-recorder/dist/index.css'
 
 interface State {
   isFocused: boolean
-  recordState: null
+  recordState: RecordState | null
   audioDataURL: string
   reset: boolean
 }
 
 class StAudioRec extends StreamlitComponentBase<State> {
-  public state = { isFocused: false, recordState: null, audioDataURL: '', reset: false}
+  public state: State = { 
+    isFocused: false, 
+    recordState: RecordState.STOP, 
+    audioDataURL: '', 
+    reset: false
+  }
 
   public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-
-    // Streamlit sends us a theme object via props that we can use to ensure
-    // that our component has visuals that match the active theme in a
-    // streamlit app.
     const { theme } = this.props
     const style: React.CSSProperties = {}
 
-    const { recordState } = this.state
-
-    // compatibility with older vers of Streamlit that don't send theme object.
     if (theme) {
-      // Use the theme object to style our button border. Alternatively, the
-      // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${
-        this.state.isFocused ? theme.primaryColor : "gray"}`
+      const borderStyling = `1px solid ${ this.state.isFocused ? theme.primaryColor : "gray"}`
       style.border = borderStyling
       style.outline = borderStyling
     }
 
     return (
-      <span>
-        <div>
-          <button id='record' onClick={this.onClick_start}>
-            Start Recording
-          </button>
-          <button id='stop' onClick={this.onClick_stop}>
-            Stop
-          </button>
-          <button id='reset' onClick={this.onClick_reset}>
-            Reset
-          </button>
+      <div>
+        <button id='record' onClick={this.onClick_toggle}>
+          {this.state.recordState === RecordState.START ? "Stop" : "🎙️ Record"}
+        </button>
 
-          <button id='continue' onClick={this.onClick_continue}>
-            Download
-          </button>
-
-          <AudioReactRecorder
-            state={recordState}
-            onStop={this.onStop_audio}
-            type='audio/wav'
-            backgroundColor='rgb(255, 255, 255)'
-            foregroundColor='rgb(255,76,75)'
-            canvasWidth={450}
-            canvasHeight={100}
-          />
-
-          <audio
-            id='audio'
-            controls
-            src={this.state.audioDataURL}
-          />
-
-        </div>
-      </span>
+        <AudioReactRecorder
+          state={this.state.recordState}
+          onStop={this.onStop_audio}
+          type='audio/wav'
+          backgroundColor='rgb(255, 255, 255)'
+          foregroundColor='rgb(255,76,75)'
+          canvasWidth={450}
+          canvasHeight={100}
+        />
+      </div>
     )
   }
 
-
-  private onClick_start = () => {
-    this.setState({
-      reset: false,
-      audioDataURL: '',
-      recordState: RecordState.START
-    })
-    Streamlit.setComponentValue('')
-  }
-
-  private onClick_stop = () => {
-    this.setState({
-      reset: false,
-      recordState: RecordState.STOP
-    })
-  }
-
-  private onClick_reset = () => {
-    this.setState({
-      reset: true,
-      audioDataURL: '',
-      recordState: RecordState.STOP
-    })
-    Streamlit.setComponentValue('')
-  }
-
-  private onClick_continue = () => {
-    if (this.state.audioDataURL !== '')
-    {
-      // get datetime string for filename
-      let datetime = new Date().toLocaleString();
-      datetime = datetime.replace(' ', '');
-      datetime = datetime.replace(/_/g, '');
-      datetime = datetime.replace(',', '');
-      var filename = 'streamlit_audio_' + datetime + '.wav';
-
-      // auromatically trigger download
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = this.state.audioDataURL;
-      a.download = filename;
-      document.body.appendChild(a);
-      a.click();
-    }
-  }
-
-  private onStop_audio = (data) => {
-    if (this.state.reset === true)
-    {
-      this.setState({
-        audioDataURL: ''
-      })
+  private onClick_toggle = () => {
+    // Toggle between START and STOP states
+    if(this.state.recordState === RecordState.START) {
+      this.setState({ reset: false, recordState: RecordState.STOP })
+    } else {
+      this.setState({ reset: false, audioDataURL: '', recordState: RecordState.START })
       Streamlit.setComponentValue('')
-    }else{
-      this.setState({
-        audioDataURL: data.url
-      })
-
-      fetch(data.url).then(function(ctx){
-        return ctx.blob()
-      }).then(function(blob){
-        // converting blob to arrayBuffer, this process step needs to be be improved
-        // this operation's time complexity scales exponentially with audio length
-        return (new Response(blob)).arrayBuffer()
-      }).then(function(buffer){
-        Streamlit.setComponentValue({
-          "arr": new Uint8Array(buffer)
-        })
-      })
-
     }
+  }
 
+  private onStop_audio = (data: { url: string }) => {
+    if (this.state.reset === true) {
+      this.setState({ audioDataURL: '' })
+      Streamlit.setComponentValue('')
+    } else {
+      this.setState({ audioDataURL: data.url })
 
+      fetch(data.url)
+        .then(ctx => ctx.blob())
+        .then(blob => (new Response(blob)).arrayBuffer())
+        .then(buffer => {
+          Streamlit.setComponentValue({ "arr": new Uint8Array(buffer) })
+        })
+    }
   }
 }
 
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
 export default withStreamlitConnection(StAudioRec)
 
-// Tell Streamlit we're ready to start receiving data. We won't get our
-// first RENDER_EVENT until we call this function.
 Streamlit.setComponentReady()
-
-// Finally, tell Streamlit to update our initial height. We omit the
-// `height` parameter here to have it default to our scrollHeight.
 Streamlit.setFrameHeight()
